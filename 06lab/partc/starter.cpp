e687e11ca6bbd971347106930784d3991975601c
Wellesley Shumway <wkshumway@Wellesleys-MacBook-Pro.local>
2017-06-29 16:47:22 -0600

    First Optimizations

--- 06lab/partc/starter.cpp
+++ 06lab/partc/starter.cpp
@@ -30,7 +30,7 @@
 *        header that says:
 *           This code is to be compiled with: CAL_COMPILE_OPT=" ... "
 *        You will change what is between the " " to have your desired options.
-*    
+*
 *    - Month numbers are 1-12
 *    - Years numbers are >= 1753
 *
@@ -40,7 +40,7 @@
 *      or if you want to add options to the g++ compile of your code:
 *         CAL_COMPILE_OPT="-Opt1 -Opt2 ..." /home/ecen324/calendarOptimize/testCalOpt -o
 *      To see more options for the test program execute:
-*         /home/ecen324/calendarOptimize/testCalOpt -h 
+*         /home/ecen324/calendarOptimize/testCalOpt -h
 *
 *    - Submit your fastest code using the "submit" command.  The code will
 *      fail to compile, but that is OK.
@@ -54,7 +54,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <iomanip>
-#include <cassert>
 
 // Get lab specific variables --You must include this #include--
 #include "/home/ecen324/calendarOptimize/lab_consts.h"
@@ -72,36 +71,25 @@ using namespace std;
 bool isLeapYear(int year)
 {
    // No leap years before 1753
-   assert(year >= yearSeed);
-   
+
    // What, are you Buck Rogers?
-   assert(year >= buckRogers || year <= buckRogers);
-   
+
    bool leap;
    // Not a leap year if not evenly divisible by 4
    if (year % 4 != 0)
-      leap = false; // 2001, 2002, 2003, 2005, etc.
-   else if (year % 4 == 0)
-      leap = true; // 2004, 2008, 2012, 2016, etc.
-   assert(year % 4 == 0 && leap || year % 4 != 0 && !leap);
-   
-   // Is a leap year if not divisible by 100
-   if (year % 100 != 0 && leap)
-      leap = true; // 1992, 1996, 2004, 2008, etc.
-   if (year % 100 == 0 && leap)
-      leap = false; // 1800, 1900, 2100, 2200, etc.
-   assert(year % 100 == 0 && !leap || year % 100 != 0);
+     return false;//   else if (year % 4 == 0)
+   //  leap = true; // 2004, 2008, 2012, 2016, etc.
+
+   // Is a leap yar if not divisible by 100
+   else if (year % 100 != 0 && leap)
+     return true;
 
    // Is a leap year if on the quad century
-   if (year % 400 == 0 && !leap || leap)
-      leap = true; // 2000, 2400, 2800, 3200, etc.
-   if (year % 400 != 0 && !leap)
-      leap = false; // 1800, 1900, 2100, 2200, etc.
-   assert(year % 400 == 0 && leap || year % 400 != 0);
-   
-   assert(year == 1500 || year == 1700 || year == 1800 || year == 1900 ||
-         year == 2100 || year == 2200 || year == 2300 || year == year);
-   return leap;
+   else if (year % 400 != 0 && !leap)
+     return false;
+
+   else
+     return false;
 }
 
 
@@ -111,50 +99,25 @@ bool isLeapYear(int year)
 int numDaysInMonth(int month, int year)
 {
    int days;
-   if (month == 1)
-      days = 31;
-   if (month == 2 && isLeapYear(year))
-      days = 29;
-   if (month == 2 && !isLeapYear(year))
-      days = 28;
-   if (month == 3)
-      days = 31;
-   if (month == 4)
-      days = 30;
-   if (month == 5)
-      days = 31;
-   if (month == 6)
-      days = 30;
-   if (month == 7)
-      days = 31;
-   if (month == 8)
-      days = 31;
-   if (month == 9)
-      days = 30;
-   if (month == 10)
-      days = 31;
-   if (month == 11)
-      days = 30;
-   if (month == 12)
-      days = 31;
+   int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-   assert(month > 0 && month < 13);
-   assert(days >= 28 && days <= 31);
-   return days;
+   if (isLeapYear(year))
+     return monthDays[month-1] + 1;
+   else
+     return monthDays[month-1];
 }
+
 
 /**********************************************************************
  * YOUR DESCRIPTION HERE
  ***********************************************************************/
 int numDaysInYear(int year)
 {
-   assert(year >= yearSeed && (year >= buckRogers || year <= buckRogers));
    int days;
    if (isLeapYear(year))
       days = 366;
    if (!isLeapYear(year))
       days = 365;
-   assert(days >= 365 && days <= 366);
    return days;
 }
 
@@ -166,26 +129,19 @@ int numDaysInYear(int year)
 int computeOffset(int month, int year)
 {
    int days = 0;
-   assert(month >= 1 && month <= 12);
-   assert(year >= yearSeed && (year >= buckRogers || year <= buckRogers));
 
    // Add up the days associated with each year
    for (int cYear = yearSeed; cYear < year; cYear++)
    {
-//      assert(numDaysInYear(cYear) == 366 ||
- //            numDaysInYear(cYear) == 365); 
       days += numDaysInYear(cYear);
    }
 
    // Add up the days associated with each month
    for (int cMonth = 1; cMonth < month; cMonth++)
    {
-      assert(numDaysInMonth(cMonth, year) >= 28 &&
-             numDaysInMonth(cMonth, year) <= 31);
       days += numDaysInMonth(cMonth, year);
    }
 
-   assert((days % 7 >= 0) && (days % 7 <= 6));
    return days % 7;
 }
 
@@ -195,8 +151,6 @@ int computeOffset(int month, int year)
 int displayHeader(int month, int year, char *calendar, int pos)
 {
    pos += sprintf(calendar + pos, "\n");
-   assert(month >= 1 && month <= 12);
-   assert(year >= yearSeed && (year >= buckRogers || year <= buckRogers));
 
    // Write the month
    if (month == 1)
@@ -237,13 +191,10 @@ int displayHeader(int month, int year, char *calendar, int pos)
  *******************************************************************/
 int displayTable(int numDays, int offset, char *calendar, int pos)
 {
-   assert(offset >= 0 && offset <= 6);
-   assert(numDays >= 28 && numDays <= 31);
-   
+
    // Special case offset==6. We don't want a blank first line
    if (offset == 6)
       offset = -1;
-   assert(offset >= -1 && offset <= 5);
 
    // Display the header
    for (int i = 0; i < 7; i++)
@@ -287,7 +238,7 @@ int displayTable(int numDays, int offset, char *calendar, int pos)
       pos += sprintf(calendar + pos, "\n");
 
    return pos;
-}  
+}
 
 
 /**********************************************************************
@@ -298,20 +249,13 @@ void display(int month, int year, char *calendar)
    int pos = 0;
    int offset = computeOffset(month, year);
 
-   assert(year >= yearSeed && (year >= buckRogers || year <= buckRogers));
-   assert(month >= 1 && month <= 12);
-   assert(offset >= 0 && offset <= 6);
-   
    // Header
    pos += displayHeader(month, year, calendar, pos);
 
    // Body of the table
-   assert(numDaysInMonth(month, year) >= 28 &&
-          numDaysInMonth(month, year) <= 31);
    pos += displayTable(numDaysInMonth(month, year), offset, calendar, pos);
 
    // Output calendars shouldn't overlap each other in calendar array buffer
-   assert(pos < CAL_LENGTH);
 
    return;
 }
